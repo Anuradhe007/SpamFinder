ebb5b7563af1cf0ef60e5c9bd4a87f545ffc5db1
anuradha <anuradhap@intervest.lk>
2018-11-29 11:43:08 +0530

    Implemented quality score calculation.

--- quality.py
+++ quality.py
@@ -1,13 +1,18 @@
-def quality_score(tp, tn, fp, fn):
-    q = tp + tn/(tp+tn+10*fp+fn)
+import utils
 
-    if 0 <= q < 0.3:
-        return 0
-    elif 0.3 <= q < 0.5:
-        return 1
-    elif 0.5 <= q < 0.7:
-        return 2
-    elif 0.7 <= q < 0.9:
-        return 2.5
-    elif 0.9 <= q < 1:
-        return 3
+
+def quality_score(tp, tn, fp, fn):
+    quality = (tp + tn) / (tp + tn + 10 * fp + fn)
+    return quality
+
+
+def compute_quality_for_corpus(corpus_dir):
+    truth_dict = utils.read_classification_from_file(corpus_dir + '!truth.txt')
+    pred_dict = utils.read_classification_from_file(corpus_dir + '!prediction.txt')
+    confusion_matrix = utils.compute_confusion_matrix(truth_dict, pred_dict)
+    tp = getattr(confusion_matrix, 'tp')
+    tn = getattr(confusion_matrix, 'tn')
+    fp = getattr(confusion_matrix, 'fp')
+    fn = getattr(confusion_matrix, 'fn')
+    quality = quality_score(tp, tn, fp, fn)
+    return quality
--- utils.py
+++ utils.py
@@ -2,21 +2,19 @@ from collections import namedtuple
 
 
 def read_classification_from_file(fpath):
-    classification_file = open(fpath, "r")
-    fileLines = classification_file.readlines()
-    classification_dict = dict()
-    for line in fileLines:
-        tmp = {line.split(None)[0]: line.split(None)[1]}
-        classification_dict.update(tmp)
-    classification_file.close()
-    return classification_dict
+    cls_file = open(fpath, "r")
+    cls_dict = dict()
+    for line in cls_file.readlines():
+        cls_dict.update({line.split(None)[0]: line.split(None)[1]})
+    cls_file.close()
+    return cls_dict
 
 
 def write_classification_to_file(cls_dict, fpath):
-    file = open(fpath, "w+")
-    for name, val in cls_dict.items():
-        file.write(name + ' ' + val + '\n')
-    file.close()
+    cls_file = open(fpath, "w+")
+    for email, tag in cls_dict.items():
+        cls_file.write(email + ' ' + tag + '\n')
+    cls_file.close()
 
 
 def compute_confusion_matrix(truth_dict, pred_dict, pos_tag='SPAM', neg_tag='OK'):
@@ -28,13 +26,13 @@ def compute_confusion_matrix(truth_dict, pred_dict, pos_tag='SPAM', neg_tag='OK'
         tn = 0
         fp = 0
         fn = 0
-        for email, stat in truth_dict.items():
-            if stat == pred_dict.get(email) and pos_tag == stat:
+        for email, tag in truth_dict.items():
+            if tag == pred_dict.get(email) and pos_tag == tag:
                 tp = tp + 1
-            if stat == pred_dict.get(email) and neg_tag == stat:
+            if tag == pred_dict.get(email) and neg_tag == tag:
                 tn = tn + 1
-            if stat != pred_dict.get(email) and pos_tag == pred_dict.get(email):
+            if tag != pred_dict.get(email) and pos_tag == pred_dict.get(email):
                 fp = fp + 1
-            if stat != pred_dict.get(email) and neg_tag == pred_dict.get(email):
+            if tag != pred_dict.get(email) and neg_tag == pred_dict.get(email):
                 fn = fn + 1
         return ConfMat(tp=tp, tn=tn, fp=fp, fn=fn)
